9a43957ee052f313b88378a204e7d5cc49bd0fea
Prasetyo Wicaksono <pras@openmailbox.org>
2018-02-15 12:34:59 +0700

    create test to resolve local driver

---
+++ tests/Filesystem/FilesystemManagerTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Illuminate\Tests\Filesystem;
+
+use Illuminate\Container\Container;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Filesystem\FilesystemManager;
+use PHPUnit\Framework\TestCase;
+
+class FilesystemManagerTest extends TestCase
+{
+    /**
+     * @var Container
+     */
+    private $app;
+
+    public function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        $this->app = new Container();
+    }
+
+    /**
+     * @test
+     */
+    public function testCreateLocalDriver()
+    {
+        $this->app['config'] = [
+            'filesystems.disks.local' => [
+                'root' => '.'
+            ]
+        ];
+
+        $manager = new FilesystemManager($this->app);
+
+        $driver = $manager->disk('local');
+
+        $this->assertInstanceOf(FilesystemAdapter::class, $driver);
+    }
+}
